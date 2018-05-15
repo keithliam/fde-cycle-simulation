92131bb3e5f503c78abe2c000eaec45619bbef2b
codeslaw <jmtgalvez@gmail.com>
2018-05-15 15:37:25 +0800

    FDEMW loop

--- main.vhdl
+++ main.vhdl
@@ -25,10 +25,12 @@ begin
 	variable l : line;
 	variable i : integer := 0;
 	variable b : std_logic_vector(0 to 31);
+	variable line_count : integer := 0;
+
 	variable stall_flag : std_logic := '0';
 	
 	variable reg_values : values;
-	variable fdemw : values;
+	variable fdemw : values; --contains which instruction is using each stage
 	variable reg_used_by : values;
 
 	variable instr : integer;
@@ -45,67 +47,74 @@ begin
 			t_mem(i) <= b;
 			i := i + 1;
 			this <= b;
+			line_count := line_count + 1;
 			wait for 10 ns;
 		end loop;
 		file_close(f);
 
 		--loops through instructions
 		i := 0; --line counter
-		while (i<=14) loop			
+		while (i<=line_count) loop			
 			--fetch
 			fdemw(0) := i;
+			stages(0) <= '1';
 			
 			--decode
-			fdemw(1) := fdemw(0) - 1;
-			instr := to_integer(unsigned(t_mem(fdemw(1))(0 to 2)));
-			dst := to_integer(signed(t_mem(fdemw(1))(4 to 8)));
-			src := to_integer(signed(t_mem(fdemw(1))(10 to 14)));
-			tar := to_integer(signed(t_mem(fdemw(1))(16 to 20)));
-			if t_mem(i)(3) = '0' then -- the first bit determines if the arg is a register or an immediate value
-				if registers(dst) = '0' then --checks if register is avaiable
-					if t_mem(i)(9) = '0' then
-						if registers(src) = '0' then
-							if (t_mem(i)(15) = '0'  & instr /= 0) then
-								if registers(tar) = '0' then
+			if fdemw(0) /= 0 & stall_flag = '0' then
+				fdemw(1) := fdemw(0) - 1;
+				stages(1) <= '1';
+				instr := to_integer(unsigned(t_mem(fdemw(1))(0 to 2)));
+				dst := to_integer(signed(t_mem(fdemw(1))(4 to 8)));
+				src := to_integer(signed(t_mem(fdemw(1))(10 to 14)));
+				tar := to_integer(signed(t_mem(fdemw(1))(16 to 20)));
+				if t_mem(i)(3) = '0' then -- the first bit determines if the arg is a register or an immediate value
+					if registers(dst) = '0' then --checks if register is avaiable
+						if t_mem(i)(9) = '0' then
+							if registers(src) = '0' then
+								if (t_mem(i)(15) = '0' & instr /= 0) then
+									if registers(tar) = '0' then
+										registers(dst) <= '1'; --set the register to unavailable
+										registers(src) <= '1'; --set the register to unavailable
+										registesr(tar) <= '1'; --set the register to unavailable
+									else
+										stall_flag := '1';
+									end if;
+								else
 									registers(dst) <= '1'; --set the register to unavailable
 									registers(src) <= '1'; --set the register to unavailable
-									registesr(tar) <= '1'; --set the register to unavailable
-								else
-									stall_flag := '1';
 								end if;
 							else
-								registers(dst) <= '1'; --set the register to unavailable
-								registers(src) <= '1'; --set the register to unavailable
+								stall_flag := '1';
 							end if;
-						else
-							stall_flag := '1';
+						else -- if second argument is an immediate value
+							registers(dst) <= '1'; --set the register to unavailable
 						end if;
-					else -- if second argument is an immediate value
-						registers(dst) <= '1'; --set the register to unavailable
+					else
+						stall_flag := '1';
 					end if;
-				else
-					stall_flag := '1';
 				end if;
 			end if;
 
 			--execute
-			fdemw(2) := fdemw(1) - 1;
-			instr := to_integer(unsigned(t_mem(fdemw(2))(0 to 2)));
-			dst := to_integer(signed(t_mem(fdemw(2))(4 to 8)));
-			src := to_integer(signed(t_mem(fdemw(2))(10 to 14)));
-			tar := to_integer(signed(t_mem(fdemw(2))(16 to 20)));
-			if instr = 0 then -- if instruction is LOAD
-				
-			elsif instr = 1 then -- if instructions is ADD
+			if fdemw(1) /= 0 then
+				fdemw(2) := fdemw(1) - 1;
+				instr := to_integer(unsigned(t_mem(fdemw(2))(0 to 2)));
+				dst := to_integer(signed(t_mem(fdemw(2))(4 to 8)));
+				src := to_integer(signed(t_mem(fdemw(2))(10 to 14)));
+				tar := to_integer(signed(t_mem(fdemw(2))(16 to 20)));
+				if instr = 0 then -- if instruction is LOAD
+					
+				elsif instr = 1 then -- if instructions is ADD
 
-			elsif instr = 2 then -- if instruction is SUB
+				elsif instr = 2 then -- if instruction is SUB
 
-			elsif instr = 3 then -- if instruction is MUL
+				elsif instr = 3 then -- if instruction is MUL
 
-			elsif instr = 4 then -- if instruction is DIV
+				elsif instr = 4 then -- if instruction is DIV
 
-			elsif instr = 5 then -- if instruction is MOD
+				elsif instr = 5 then -- if instruction is MOD
 
+				end if;
 			end if;
 
 			--memory
@@ -121,12 +130,14 @@ begin
 				end if;
 			end loop;
 
+			wait for 5 ns;
+
 			if stall_flag = '1' then
 				i := i - 1;
 			end if;
 
 			program_counter <= std_logic_vector(unsigned(program_counter) + 1);
-			wait for 10 ns;
+			wait for 5 ns;
 		end loop;
 
 		wait;
